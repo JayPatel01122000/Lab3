3d2533f746abbd7615a4239d5707690fbe74b01c
Jay Patel <200426940@student.georgianc.on.ca>
2020-01-23 22:26:18 -0500

    completed Lab3

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var Phone1 = new List<Phone>
+            {
+                new Phone {PhoneName = "Iphone 11", ScreenSize = 120, Manufacturer = "Apple", DateReleased = new DateTime(2019,12,02), MSRP = 120000},
+                new Phone {PhoneName = "Samsung Galaxy S9", ScreenSize = 115, Manufacturer = "Samsung", DateReleased = new DateTime(2015,10,03), MSRP = 65000},
+                new Phone {PhoneName = "One plus 6", ScreenSize = 100, Manufacturer = "One Plus", DateReleased = new DateTime(2019,02,01), MSRP = 90000},
+            };
+            return View(Phone1);
+        }
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {  
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            int PhoneID = 0;
+
+            DateReleased = new DateTime();
+        }
+        public int PhoneID { get; set; }
+        public String PhoneName { get; set; }
+        public String Manufacturer { get; set; }
+        public float MSRP { get; set; }
+        public int ScreenSize{ get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
